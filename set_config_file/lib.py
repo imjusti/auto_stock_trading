137d444e985389b9827fa58a4dd233a15da810a2
imjusti <imjusti77@gmail.com>
2022-12-03 21:47:44 +0900

    조건5 추가

--- set_config_file/lib.py
+++ set_config_file/lib.py
@@ -30,18 +30,19 @@ def getCase8(url):
     result = soup.find(id='result')
     return int(result.string)
 
-# 조건1,7 예측정보 가져오기
-def getCaseFromGSpread(worksheet, str_today, case1_colname, case7_colname):
+# 조건1,5,7 예측정보 가져오기
+def getCaseFromGSpread(worksheet, str_today, case1_colname, case5_colname, case7_colname):
     cell = worksheet.find(str_today)
     result = None
     if cell is not None:
         case1 = int(worksheet.acell(case1_colname + str(cell.row)).value)
+        case5 = int(worksheet.acell(case5_colname + str(cell.row)).value)
         case7 = int(worksheet.acell(case7_colname + str(cell.row)).value)
-        result = (case1, case7)
+        result = (case1, case5, case7)
     return result
 
 # 전략 결정
-def decideStrategy(case1, case7, case8):
+def decideStrategy(case1, case5, case7, case8):
     # 방향(0: 하락, 1: 상승, -1: 휴업)
     dirToday = -1
     # 매도시점(1: 10시, 2: 종가)
@@ -65,20 +66,22 @@ def write2StrategyFile(str_today, buy_time, sell_time, stock_code, file_path):
         json.dump(data, outfile, indent=2)
         
 # 메시지 생성
-def makeMessage(str_today, dirToday, sellType, case1, case7, case8, worksheet):
+def makeMessage(str_today, dirToday, sellType, case1, case5, case7, case8, worksheet):
     msg = '금일휴업'
     if dirToday > -1:
         if sellType == 1: msg = '10시매도'
         elif sellType == 2: msg = '종가매도'
         msg += ', 방향: ' + str(dirToday)
 
-    msg_telegram = '날짜: ' + str_today+ '\n'
+    msg_telegram = ''
+    msg_telegram += '날짜: ' + str_today+ '\n'
     msg_telegram += '[오늘의 작전] ' + msg + '\n'
     msg_telegram += '\n'
 
     # 각 조건값들 전송
     msg_telegram += '[참고 조건값들]\n'
     msg_telegram += '조건1: ' + str(case1) + '\n'
+    msg_telegram += '조건5: ' + str(case5) + '\n'
     msg_telegram += '조건7: ' + str(case7) + '\n'
     msg_telegram += '조건8: ' + str(case8) + '\n'
     msg_telegram += '\n'
--- set_config_file/run.py
+++ set_config_file/run.py
@@ -26,18 +26,19 @@ print('\ndate: ' + str_today)
 case8 = lib.getCase8(config['case8_url'])
 print('조건8', case8)
 
-# 조건1,7 예측정보 가져오기
+# 조건1,5,7 예측정보 가져오기
 worksheet = lib.getGSpread(config['google_spreadsheet_url'], config['google_spreadsheet_worksheet'])
-result = lib.getCaseFromGSpread(worksheet, str_today, config['google_spreadsheet_case1_colname'], config['google_spreadsheet_case7_colname'])
+result = lib.getCaseFromGSpread(worksheet, str_today, config['google_spreadsheet_case1_colname'], config['google_spreadsheet_case5_colname'], config['google_spreadsheet_case7_colname'])
 if result is None:
     lib.sendTelegramMsg(bot, cfg_telegram['chat_id'], '[오류] 엑셀에서 ' + str_today + ' 데이터를 가져오지 못했습니다.')
     exit()
-case1, case7 = result
+case1, case5, case7 = result
 print('조건1', case1)
+print('조건5', case5)
 print('조건7', case7)
 
 # 오늘의 방향 결정
-dirToday, sellType = lib.decideStrategy(case1, case7, case8)
+dirToday, sellType = lib.decideStrategy(case1, case5, case7, case8)
 
 if dirToday > -1:
     etime = config['sell_time']
@@ -50,5 +51,5 @@ if dirToday > -1:
     lib.write2StrategyFile(str_today, config['buy_time'], etime, stock_code, config['output_path'])
 
 # 텔레그램으로 오늘의 작전 전송
-msg_telegram = lib.makeMessage(str_today, dirToday, sellType, case1, case7, case8, worksheet)
+msg_telegram = lib.makeMessage(str_today, dirToday, sellType, case1, case5, case7, case8, worksheet)
 lib.sendTelegramMsg(bot, cfg_telegram['chat_id'], msg_telegram)
