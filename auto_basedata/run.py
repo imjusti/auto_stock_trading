61d74a1a4e30027a6eb061013958f3a5a693ba06
imjusti <imjusti77@gmail.com>
2023-06-21 16:28:12 +0900

    결과 텔레그램 전송 추가

--- auto_basedata/run.py
+++ auto_basedata/run.py
@@ -1,13 +1,29 @@
 import requests
 import json
 import os
+import telegram
+import asyncio
+
+# 텔레그램 전송
+async def sendTelegramMsg(title, bot, chat_id, msg):
+    last_msg = '** ' + title + ' **\n'
+    last_msg += msg
+    await bot.sendMessage(chat_id=chat_id, text=last_msg)
 
 
 # 실행경로
 path = os.path.dirname(os.path.realpath(__file__))
+
 # 설정파일 읽기
 with open(path + '/config.json', 'r', encoding='utf-8') as f:
     config = json.load(f)
 
-req = requests.get(config['url'])
-print(config['url'], req)
+# 텔레그램 설정파일 읽기
+with open(path + '/telegram.json') as f:
+    cfg_telegram = json.load(f)
+bot = telegram.Bot(token=cfg_telegram['token'])
+
+res = requests.get(config['url'])
+print(config['url'], res.text)
+
+asyncio.run(sendTelegramMsg('get_basedata', bot, cfg_telegram['chat_id'], res.text))
