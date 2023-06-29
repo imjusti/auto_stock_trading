f802a563242354db6e169b5338c5f8aee1be4e6d
imjusti <imjusti77@gmail.com>
2023-06-29 16:34:54 +0900

    조건10 계산시 조건7대신 조건5사용하게 변경

--- set_config_file/lib.py
+++ set_config_file/lib.py
@@ -89,7 +89,7 @@ def decideStrategy(trading_type, cases):
         if cases['조건8'] == cases['조건1']:
             dirToday = cases['조건8']
             sellType = 1
-        elif cases['조건8'] == cases['조건7']:
+        elif cases['조건8'] == cases['조건5']:
             dirToday = cases['조건8']
             sellType = 2
     # 조건11
