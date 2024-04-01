556c6067bfef713fd5c72765bbe82550910b6109
imjusti <imjusti77@gmail.com>
2024-04-01 13:52:57 +0900

    필터추가 및 코드 최적화

--- set_config_file/lib.py
+++ set_config_file/lib.py
@@ -15,18 +15,7 @@ def getCase8(url):
     result = soup.find(id='result')
     return int(result.string)
 
-# 조건1,5,7 예측정보 가져오기
-def getCaseFromGSpread(worksheet, str_today, case1_colname, case5_colname, case7_colname):
-    cell = worksheet.find(str_today)
-    result = None
-    if cell is not None:
-        case1 = int(worksheet.acell(case1_colname + str(cell.row)).value)
-        case5 = int(worksheet.acell(case5_colname + str(cell.row)).value)
-        case7 = int(worksheet.acell(case7_colname + str(cell.row)).value)
-        result = {'조건1': case1, '조건5': case5, '조건7': case7}
-    return result
-
-# 조건1,2,5,7 예측정보 가져오기
+# 조건1,2,5,7,필터1 예측정보 가져오기
 def getStrategy(url, str_today):
     res = requests.get(url + str_today)
     obj = res.json();
@@ -34,60 +23,50 @@ def getStrategy(url, str_today):
     case1 = int(obj['case1'])
     case5 = int(obj['case5'])
     case7 = int(obj['case7'])
+    filter1 = int(obj['filter1'])
 
     case2 = ''
     if obj['case2'] is not None: case2 = int(obj['case2'])
         
-    result = {'조건1': case1, '조건2': case2, '조건5': case5, '조건7': case7}
+    result = {'조건1': case1, '조건2': case2, '조건5': case5, '조건7': case7, '필터1': filter1}
     return result
 
 # 전략 결정
-def decideStrategy(trading_type, cases):
+def decideStrategy(trading_type0, cases):
+    result = trading_type0.split("_")
+    trading_type = result[0] if result else ''
+    trading_filter = result[1] if len(result) > 1 else ''
+    
     # 방향(0: 하락, 1: 상승, -1: 휴업)
     dirToday = -1
     # 매도시점(1: 10시, 2: 종가)
-    sellType = 1
+    sellType = 2
 
     # 조건1
-    if trading_type == 1:
-        dirToday = cases['조건1']
-        sellType = 2
+    if trading_type == '1': dirToday = cases['조건1']
     # 조건5
-    elif trading_type == 5:
-        dirToday = cases['조건5']
-        sellType = 2
+    elif trading_type == '5': dirToday = cases['조건5']
     # 조건6
-    elif trading_type == 6:
-        if cases['조건5'] == cases['조건1']:
-            dirToday = cases['조건1']
-            sellType = 2
+    elif trading_type == '6':
+        if cases['조건5'] == cases['조건1']:  dirToday = cases['조건1']
     # 조건7
-    elif trading_type == 7:
-        dirToday = cases['조건7']
-        sellType = 2
+    elif trading_type == '7': dirToday = cases['조건7']
     # 조건8
-    elif trading_type == 8:
-        dirToday = cases['조건8']
-        sellType = 2
+    elif trading_type == '8': dirToday = cases['조건8']
     # 조건9
     # 23.09.26 종가매도로 변경
-    elif trading_type == 9:
-        if cases['조건8'] == cases['조건2']:
-            dirToday = cases['조건8']
-            sellType = 2
+    elif trading_type == '9':
+        if cases['조건8'] == cases['조건2']: dirToday = cases['조건8']
     # 조건11
-    elif trading_type == 11:
-        if cases['조건8'] == cases['조건5']:
-            dirToday = cases['조건8']
-            sellType = 2
+    elif trading_type == '11':
+        if cases['조건8'] == cases['조건5']: dirToday = cases['조건8']
     # 조건12
-    elif trading_type == 12:
-        if cases['조건8'] == cases['조건5']:
-            dirToday = cases['조건8']
-            sellType = 2
-        elif cases['조건8'] == cases['조건2']:
-            dirToday = cases['조건8']
-            sellType = 2
+    elif trading_type == '12':
+        if cases['조건8'] == cases['조건5']: dirToday = cases['조건8']
+        elif cases['조건8'] == cases['조건2']: dirToday = cases['조건8']
+
+    # 필터 확인
+    if trading_filter and cases['필터' + trading_filter] != '1': dirToday = -1
 
     return (dirToday, sellType)
 
@@ -124,6 +103,7 @@ def makeMessage(str_today, dirToday, sellType, cases, fundName):
     msg_telegram += '조건5: ' + str(cases['조건5']) + '\n'
     msg_telegram += '조건7: ' + str(cases['조건7']) + '\n'
     msg_telegram += '조건8: ' + str(cases['조건8']) + '\n'
+    msg_telegram += '필터1: ' + str(cases['필터1']) + '\n'
     msg_telegram += '\n'
     
     return msg_telegram
